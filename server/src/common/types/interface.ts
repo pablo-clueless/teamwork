7ba8c0c3ab578e9bc1d77792e3fb8c3c172a419d
tdadadavid <davidtofunmidada@gmail.com>
2023-01-21 22:37:20 +0100

    add interface that will be used in the app

--- server/src/common/types/interface.ts
+++ server/src/common/types/interface.ts
@@ -1,5 +1,13 @@
 import { ObjectSchema } from "joi";
+import { IncomingHttpHeaders } from "http";
 
+export interface ControllerArgs {
+    body: any;
+    params: any;
+    query: any;
+    files:  any | null | undefined;
+    headers: IncomingHttpHeaders,
+}
 
 export interface ValidationSchema {
     bodySchema?: ObjectSchema;
