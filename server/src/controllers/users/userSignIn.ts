c2d96347726aab6f74800b1e0abff38d6fccc3d6
tdadadavid <davidtofunmidada@gmail.com>
2023-01-21 22:33:09 +0100

    add controller for user to sign in

---
+++ server/src/controllers/users/userSignIn.ts
@@ -0,0 +1,32 @@
+import HTTP from "../../common/constants/http";
+import { ConflictError } from "../../common/errors/conflictError";
+import { hashData } from "../../common/helpers";
+import { ControllerArgs } from "../../common/types";
+import { userModel } from "../../models";
+
+
+
+export const signIn = async ({ body }: ControllerArgs) => {
+    const { email, name, password, username } = body;
+
+    const userExists = await userModel.find({ email });
+    if(userExists) throw new ConflictError("user already exists");
+
+    const hashPassword = hashData(password);
+
+    const user = new userModel({ 
+        email,
+        name,
+        password: hashPassword,
+        username,
+    });
+
+    await user.save()
+
+    return {
+        status: HTTP.CREATED,
+        message: "User registered sucessfully",
+        user,
+    }
+    
+}
